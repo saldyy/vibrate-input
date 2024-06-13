e8e87b0124ca5b9621cc3de471354c02762293cc
Phillip Nguyen <png9981@gmail.com>
2024-06-13 19:55:34 +0700

    feat: extract tailwind class

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ChangeEvent, useEffect, useState } from "react";
+import { ChangeEvent, useState } from "react";
 
 export default function Home() {
   const [input, setInput] = useState("");
@@ -18,7 +18,7 @@ export default function Home() {
   return (
     <main className="m-auto w-screen h-screen flex">
       <div className="m-auto">
-        <input className={`border-2 rounded-md text-black focus:outline-none ${isValid ? 'border-blue-600' : 'border-red-600 vibrate'} `} onChange={onTextChange} />
+        <input className={`border-2 rounded-md text-black focus:outline-none ${isValid ? 'input-default' : 'input-error'} `} onChange={onTextChange} />
         <button className="ml-5" onMouseDown={onSubmit}>Submit</button>
       </div>
     </main>
